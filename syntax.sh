5403506a6c56b7d8c634086b7f2765c87befb3fc
cloudmoh <mohammed830049@outlook.com>
2023-09-24 13:04:05 +0000

    This example for syntax fo all statements

---
+++ syntax.sh
@@ -0,0 +1,24 @@
+# while loop syntax
+
+while <condition>
+do
+	<command 1>
+	<command 2>
+done
+
+
+# for loop syntax
+
+for <var> in <value1 value2 ... valuen>
+do 
+	<command 1>
+	<command 2>
+done
+
+#until statement
+
+until <condition>
+do
+	<command 1>
+	<command 2>
+done
